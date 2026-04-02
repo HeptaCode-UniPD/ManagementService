3780cf2ff706e0596b4caac522c21b2d76017299
amerigovegliante <veglianteamerigo583@gmail.com>
2026-04-02 11:51:16 +0200

    minor docker fixes

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: 'GET,POST,DELETE,PUT,PATCH',
     allowedHeaders: 'Content-Type, Authorization',
   });
